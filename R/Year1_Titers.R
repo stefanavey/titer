adf334cbd5a5dab87d741cc884dabb46c687a161
spa23 <stefan.avey@yale.edu>
2016-11-21 11:55:44 -0500

    Added titer data

---
+++ R/Year1_Titers.R
@@ -0,0 +1,11 @@
+#' Year 1 titers.
+#' 
+#' Antibody titers to 3 strains of influenza in a cohort of young and older adults from Yale during the 2010-2011 flu season.
+#' 
+#' @docType data
+#' @format a data frame instance
+#' @author Stefan Avey
+#' @examples
+#' head(Year1_Titers)
+NULL
+
